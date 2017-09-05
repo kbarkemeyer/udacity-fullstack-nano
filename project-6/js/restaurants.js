9c4839a9d86c472d9f722fe835abed20b818c3ef
Karen Barkemeyer <karenbarkemeyer@gmail.com>
2017-09-05 15:46:36 -0700

    Add Google Maps asynchronous loading and error handling

---
+++ project-6/js/restaurants.js
@@ -0,0 +1,86 @@
+/**
+    * Model holding intial restaurant data, input for location filter and API key.
+    **/
+
+
+var restaurants = [
+
+        {
+            name: "Teote",
+            location: "SE",
+            latitude: 45.511229,
+            longitude: -122.653820
+        },
+        {
+            name: "Ground Breaker Brewing",
+            location: "SE",
+            latitude: 45.508344,
+            longitude: -122.658558
+        },
+        {
+            name: "Harlow",
+            location: "SE",
+            latitude: 45.511968,
+            longitude: -122.626219
+        },
+        {
+            name: "Cultured Caveman",
+            location: "NW",
+            latitude: 45.582923,
+            longitude: -122.687067
+        },
+        {
+            name: "Verde Cocina",
+            location: "SW",
+            latitude: 45.4775825,
+            longitude: -122.699932
+        },
+        {
+            name: "A.N.D Cafe",
+            location: "NE",
+            latitude: 45.522738,
+            longitude: -122.607904
+        },
+        {
+            name: "Mi Metro Mole",
+            location: "NW",
+            latitude: 45.5236601,
+            longitude: -122.675150
+        },
+        {
+            name: "Dick\'s Kitchen",
+            location: "SE",
+            latitude: 45.527876,
+            longitude: -122.694337
+        },
+        {
+            name: "Besaws",
+            location: "NW",
+            latitude: 45.5339005,
+            longitude: -122.695046
+        },
+        {
+            name: "Tin Shed",
+            location: "NE",
+            latitude: 45.559009,
+            longitude: -122.650898
+        },
+        {
+            name: "White Owl Social Club",
+            location: "SE",
+            latitude: 45.513513,
+            longitude: -122.657980
+        },
+        {
+            name: "Andina",
+            location: "NW",
+            latitude: 45.526379,
+            longitude: -122.684614
+        },
+        {
+            name: "Departure",
+            location: "SW",
+            latitude: 45.519003,
+            longitude: -122.677819
+        }
+    ];
